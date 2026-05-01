0884262c8aa9c4cf14144da8e883db33bfb9372b
yu-suke0919 <yusukekk919@gmail.com>
2026-05-01 10:22:23 +0900

    employeesコントローラーをコマンドにて作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "employees/index"
+  get "employees/show"
+  get "employees/new"
+  get "employees/edit"
   devise_for :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
