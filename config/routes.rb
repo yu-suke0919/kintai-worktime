6cb54a425474bcaa393d211e056c866e41b66148
yu-suke0919 <yusukekk919@gmail.com>
2026-05-01 10:35:31 +0900

    rootの遷移先が誤っていたため修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get "employees/index"
-  get "employees/show"
-  get "employees/new"
-  get "employees/edit"
+  root to: "employees#index"
+  resource :employees
   devise_for :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
