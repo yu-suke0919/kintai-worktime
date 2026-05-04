12d9e3270c1a9e681e6709af054f3aaac4835637
yu-suke0919 <yusukekk919@gmail.com>
2026-05-04 11:12:47 +0900

    modelテストを追加

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) { FactoryBot.create(:employee) }
+  describe '新規登録' do
+    it 'FactoryBotでのユーサー登録が可能かどうか' do
+      expect(user1).to be_valid
+    end
+  end
 end
+
+
+
+
+# require 'rails_helper'
+
+# RSpec.describe Article, type: :model do
+#   let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'emailA') }
+#   let(:article1) { FactoryBot.create(:article, user: user_a, title: 'title1') }
+#   let(:article1_dup) { FactoryBot.build(:article, user: user_a, title: 'title1') }
+#   let(:article2) { FactoryBot.build(:article, user: user_a, title: 'title2') }
+#   describe 'title' do
+#     it 'タイトルが重複していなければ、どちらもsave可能' do
+#       expect(article1).to be_valid
+#       expect(article2.save).to eq true
+#     end
+
+#     it 'タイトルが重複している場合、後者はセーブ不可' do
+#       expect(article1).to be_valid
+#       expect(article1_dup.save).to eq false
+#     end
+#   end
+# end
