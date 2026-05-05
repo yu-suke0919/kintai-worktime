ea3739250b4032820d041ff2abcbc046af5e1ea4
yu-suke0919 <yusukekk919@gmail.com>
2026-05-05 11:44:41 +0900

    enum定義を追記

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
+  enum :role, { general: 0, manager: 1, admin: 2 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
