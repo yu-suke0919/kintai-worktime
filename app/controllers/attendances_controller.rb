1f89bcac977e1de8991cafc94872132372f09f25
yu-suke0919 <yusukekk919@gmail.com>
2026-05-07 14:04:29 +0900

    コントローラーに各ビューの処理を追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,35 @@
 class AttendancesController < ApplicationController
+  before_action :authenticate_employee!
+  before_action :owner_or_admin_required
+  before_action :set_attendance, only: [ :show, :update ]
   def index
+    @attendances = Attendance.find_by(employee_id: params[:id])
   end
 
   def show
   end
+  def update
+    column_name = params.require(:column_name)
 
-  def edit
+    allowed_column = {
+      "word_started_at" =>:started_at,
+      "work_finished_at" => :finished_at,
+      "break_started_at" =>:break_started_at,
+      "break_finished_at" => :break_finished_at
+    }
+    target_column = allowed_column[column_name]
+    raise ActionController::BadRequest if target_column.nil?
+
+    @attendance.update!(target_column => Time.current)
+  end
+
+  private
+
+  def set_attendance
+    @attendance = Attendance.find_or_initialize_by(employee_id: params[:id], worked_on: params[:data])
+  end
+
+  def attendance_params
+    params.require(:attendance).permit(:name, :description, :image)
   end
 end
