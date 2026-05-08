5da8dea01e2fa81a9decf225406ef528191a7037
yu-suke0919 <yusukekk919@gmail.com>
2026-05-08 11:49:55 +0900

    勤怠実績表示機能と勤怠登録機能を追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -8,26 +8,29 @@ class AttendancesController < ApplicationController
   end
 
   def show
+    @employee = Employee.find(params[:employee_id])
   end
   def update
     column_name = params.require(:column_name)
 
     allowed_column = {
-      "word_started_at" =>:started_at,
-      "work_finished_at" => :finished_at,
-      "break_started_at" =>:break_started_at,
+      "started_at" => :started_at,
+      "finished_at" => :finished_at,
+      "break_started_at" => :break_started_at,
       "break_finished_at" => :break_finished_at
     }
     target_column = allowed_column[column_name]
     raise ActionController::BadRequest if target_column.nil?
 
     @attendance.update!(target_column => Time.current)
+    @employee = Employee.find(@attendance.employee_id)
+    redirect_to employee_attendance_path(@employee, Date.current)
   end
 
   private
 
   def set_attendance
-    @attendance = Attendance.find_or_initialize_by(employee_id: params[:id], worked_on: params[:date])
+    @attendance = Attendance.find_or_create_by(employee_id: params[:employee_id], worked_on: params[:worked_on])
   end
 
   def owner_or_admin_required
