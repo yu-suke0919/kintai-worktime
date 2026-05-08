ad83f3e22474251a91a46ae4e29933e0eb8d8732
yu-suke0919 <yusukekk919@gmail.com>
2026-05-08 14:23:00 +0900

    時差計算をtokyoを基準に設定

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module KintaiWorktime
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
