42d9c434805979ac85796f12411ee757fc01dc9f
yu-suke0919 <yusukekk919@gmail.com>
2026-05-12 10:43:12 +0900

    attendanceを取得するヘルパーメソッドを追加

--- app/controllers/attendance_edit_requests_controller.rb
+++ app/controllers/attendance_edit_requests_controller.rb
@@ -1,4 +1,5 @@
 class AttendanceEditRequestsController < ApplicationController
+  before_action current_attendance only: [ :show, :new, :edit, :create, :update ]
   def index
   end
 
@@ -15,5 +16,11 @@ class AttendanceEditRequestsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def current_attendance
+    @attendance = Attendance.find_by(employee_id: params[:employee_id], worked_on: params[:worked_on])
   end
 end
