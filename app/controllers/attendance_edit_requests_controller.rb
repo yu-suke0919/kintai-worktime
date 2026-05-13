53ac1c3f9750a98f1a7f02e3a9834dadfd90ff38
yu-suke0919 <yusukekk919@gmail.com>
2026-05-13 12:17:50 +0900

    createページを作成

--- app/controllers/attendance_edit_requests_controller.rb
+++ app/controllers/attendance_edit_requests_controller.rb
@@ -11,6 +11,13 @@ class AttendanceEditRequestsController < ApplicationController
   end
 
   def create
+    @edit_request = @attendance.attendance_edit_requests.new(edit_request_params)
+    @edit_request.employee_id = @employee.id
+    if @edit_request.save
+      redirect_to employee_attendances_path, notice: "勤怠修正申請を完了しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -24,5 +31,9 @@ class AttendanceEditRequestsController < ApplicationController
   def set_attendance
     @employee = current_employee
     @attendance = @employee.attendances.find_by(worked_on: params[:attendance_worked_on])
+  end
+
+  def edit_request_params
+    params.required(:attendance_edit_request).permit(:requested_started_at, :requested_finished_at, :requested_break_started_at, :requested_break_finished_at)
   end
 end
