b7af94cf50d94bcf6d6fddf984816a4694e9bdcc
yu-suke0919 <yusukekk919@gmail.com>
2026-05-19 10:39:57 +0900

    テストを修正

--- spec/requests/attendance_edit_requests_spec.rb
+++ spec/requests/attendance_edit_requests_spec.rb
@@ -4,6 +4,15 @@ RSpec.describe "AttendanceEditRequests", type: :request do
   let(:user_1) { FactoryBot.create(:employee) }
   let(:user_2) { FactoryBot.create(:employee, email: "user2@email",) }
   let(:user_manager) { FactoryBot.create(:employee, email: "manager@email", role: :manager) }
+  before do
+    sign_in user_1
+    get employee_attendance_path(user_1, '2026-05-01')
+    sign_out user_1
+    sign_in user_2
+    get employee_attendance_path(user_2, '2026-05-01')
+    sign_out user_2
+  end
+
   shared_examples "redirect_to_login_page" do
     it "ログインページにリダイレクトされ、alertが設定されること" do
       request_action
@@ -31,38 +40,32 @@ RSpec.describe "AttendanceEditRequests", type: :request do
 
   context "非ログイン時" do
     describe "GET /index" do
-      let(:request_action) { get employee_attendances_path(user_1) }
-      it_behaves_like "redirect_to_login_page"
-    end
-
-    describe "GET /show" do
-      let(:request_action) { get employee_attendance_path(user_1, Date.current) }
+      let(:request_action) { get new_employee_attendance_attendance_edit_request_path(user_1, '2026-05-01') }
       it_behaves_like "redirect_to_login_page"
     end
   end
+
   context "ユーザー1がログイン時" do
     before do
       sign_in user_1
     end
-    describe "GET user_1/attendances/index" do
-      let(:request_action) { get employee_attendances_path(employee_id: user_1) }
+    describe "GET user_1/attendance_edit_request/index" do
+      let(:request_action) { get employee_attendance_edit_requests_path(user_1) }
       let(:logged_in_employee) { user_1 }
       it_behaves_like "have_http_status_success"
     end
-
-    describe "GET user_1/attendances/show" do
-      let(:request_action) { get employee_attendance_path(employee_id: user_1, worked_on: Date.today) }
+    describe "GET user_1/attendances/2026-05-01/attendance_edit_request/new" do
+      let(:request_action) { get new_employee_attendance_attendance_edit_request_path(user_1, '2026-05-01') }
       let(:logged_in_employee) { user_1 }
       it_behaves_like "have_http_status_success"
     end
-    describe "GET user_2/attendances/index" do
-      let(:request_action) { get employee_attendances_path(employee_id: user_2) }
+    describe "GET user_2/attendance_edit_request/index" do
+      let(:request_action) { get employee_attendance_edit_requests_path(user_2) }
       let(:logged_in_employee) { user_1 }
       it_behaves_like "redirect_to_current_employee_attendance_index_page"
     end
-
-    describe "GET user_2/attendances/show" do
-      let(:request_action) { get employee_attendance_path(employee_id: user_2, worked_on: Date.today) }
+    describe "GET user_2/attendances/2026-05-01/attendance_edit_request/new" do
+      let(:request_action) { get new_employee_attendance_attendance_edit_request_path(user_2, '2026-05-01') }
       let(:logged_in_employee) { user_1 }
       it_behaves_like "redirect_to_current_employee_attendance_index_page"
     end
@@ -72,16 +75,15 @@ RSpec.describe "AttendanceEditRequests", type: :request do
     before do
       sign_in user_manager
     end
-    describe "GET user_1/attendances/index" do
-      let(:request_action) { get employee_attendances_path(employee_id: user_1) }
+    describe "GET user_1/attendance_edit_request/index" do
+      let(:request_action) { get employee_attendance_edit_requests_path(user_1) }
       let(:logged_in_employee) { user_manager }
-      it_behaves_like "have_http_status_success"
+      it_behaves_like "redirect_to_current_employee_attendance_index_page"
     end
-
-    describe "GET user_1/attendances/show" do
-      let(:request_action) { get employee_attendance_path(employee_id: user_1, worked_on: Date.today) }
+    describe "GET user_1/attendances/2026-05-01/attendance_edit_request/new" do
+      let(:request_action) { get new_employee_attendance_attendance_edit_request_path(user_1, '2026-05-01') }
       let(:logged_in_employee) { user_manager }
-      it_behaves_like "have_http_status_success"
+      it_behaves_like "redirect_to_current_employee_attendance_index_page"
     end
   end
 end
