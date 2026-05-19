5179036a22be85a8ea01c84346f0d31d969f6aa8
yu-suke0919 <yusukekk919@gmail.com>
2026-05-19 09:57:10 +0900

    勤怠修正画面のテスト

--- spec/requests/attendance_edit_requests_spec.rb
+++ spec/requests/attendance_edit_requests_spec.rb
@@ -1,45 +1,135 @@
 require 'rails_helper'
 
 RSpec.describe "AttendanceEditRequests", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/attendance_edit_requests/index"
+  let(:user_1) { FactoryBot.create(:employee) }
+  let(:user_2) { FactoryBot.create(:employee, email: "user2@email",) }
+  let(:user_manager) { FactoryBot.create(:employee, email: "manager@email", role: :manager) }
+  shared_examples "redirect_to_login_page" do
+    it "ログインページにリダイレクトされ、alertが設定されること" do
+      request_action
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(new_employee_session_path)
+      expect(flash[:alert]).to be_present
+    end
+  end
+
+  shared_examples "redirect_to_current_employee_attendance_index_page" do
+    it "ログイン中の従業員勤怠画面にリダイレクトされ、alertが設定されること" do
+      request_action
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(employee_attendances_path(logged_in_employee))
+      expect(flash[:alert]).to be_present
+    end
+  end
+
+  shared_examples "have_http_status_success" do
+    it "HTTPリクエストステータスが、成功となること" do
+      request_action
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/attendance_edit_requests/show"
-      expect(response).to have_http_status(:success)
+  context "非ログイン時" do
+    describe "GET /index" do
+      let(:request_action) { get employee_attendances_path(user_1) }
+      it_behaves_like "redirect_to_login_page"
+    end
+
+    describe "GET /show" do
+      let(:request_action) { get employee_attendance_path(user_1, Date.current) }
+      it_behaves_like "redirect_to_login_page"
+    end
+  end
+  context "ユーザー1がログイン時" do
+    before do
+      sign_in user_1
+    end
+    describe "GET user_1/attendances/index" do
+      let(:request_action) { get employee_attendances_path(employee_id: user_1) }
+      let(:logged_in_employee) { user_1 }
+      it_behaves_like "have_http_status_success"
+    end
+
+    describe "GET user_1/attendances/show" do
+      let(:request_action) { get employee_attendance_path(employee_id: user_1, worked_on: Date.today) }
+      let(:logged_in_employee) { user_1 }
+      it_behaves_like "have_http_status_success"
+    end
+    describe "GET user_2/attendances/index" do
+      let(:request_action) { get employee_attendances_path(employee_id: user_2) }
+      let(:logged_in_employee) { user_1 }
+      it_behaves_like "redirect_to_current_employee_attendance_index_page"
+    end
+
+    describe "GET user_2/attendances/show" do
+      let(:request_action) { get employee_attendance_path(employee_id: user_2, worked_on: Date.today) }
+      let(:logged_in_employee) { user_1 }
+      it_behaves_like "redirect_to_current_employee_attendance_index_page"
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/attendance_edit_requests/new"
-      expect(response).to have_http_status(:success)
+  context "マネージャーがログイン時" do
+    before do
+      sign_in user_manager
     end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/attendance_edit_requests/create"
-      expect(response).to have_http_status(:success)
+    describe "GET user_1/attendances/index" do
+      let(:request_action) { get employee_attendances_path(employee_id: user_1) }
+      let(:logged_in_employee) { user_manager }
+      it_behaves_like "have_http_status_success"
     end
-  end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/attendance_edit_requests/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/attendance_edit_requests/update"
-      expect(response).to have_http_status(:success)
+    describe "GET user_1/attendances/show" do
+      let(:request_action) { get employee_attendance_path(employee_id: user_1, worked_on: Date.today) }
+      let(:logged_in_employee) { user_manager }
+      it_behaves_like "have_http_status_success"
     end
   end
 end
+
+
+
+# require 'rails_helper'
+
+# RSpec.describe "AttendanceEditRequests", type: :request do
+#   describe "GET /index" do
+#     it "returns http success" do
+#       get "/attendance_edit_requests/index"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /show" do
+#     it "returns http success" do
+#       get "/attendance_edit_requests/show"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /new" do
+#     it "returns http success" do
+#       get "/attendance_edit_requests/new"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /create" do
+#     it "returns http success" do
+#       get "/attendance_edit_requests/create"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /edit" do
+#     it "returns http success" do
+#       get "/attendance_edit_requests/edit"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET /update" do
+#     it "returns http success" do
+#       get "/attendance_edit_requests/update"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
