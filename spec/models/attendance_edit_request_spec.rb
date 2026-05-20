cba0312caa1f724dc94215ecec55e1533a448059
yu-suke0919 <yusukekk919@gmail.com>
2026-05-20 11:39:50 +0900

    Modelテスト追加_関連についてのテストを追加

--- spec/models/attendance_edit_request_spec.rb
+++ spec/models/attendance_edit_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AttendanceEditRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "employeeに所属しているかどうか" do
+    is_expected.to belong_to(:employee)
+  end
+  it "attendanceに所属しているかどうか" do
+    is_expected.to belong_to(:attendance)
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,14 +5,27 @@ RSpec.describe Employee, type: :model do
   let(:user_2_manager) { FactoryBot.create(:employee, email: "test2@gmail.com", role: 1) }
   let(:user_3_admin) { FactoryBot.create(:employee, email: "test3@gmail.com", role: 2) }
   let(:user_99_super_super_admin) { FactoryBot.create(:employee, email: "test@99gmail.com", role: 99) }
+
+  it "manager(Employee)に所属しているかどうか" do
+    is_expected.to belong_to(:manager).class_name("Employee").optional
+  end
+
+  it "部下(subordinates)をmanager_id(自身のid)で複数持ち、削除時は各部下のmanager_idをNULLにすること" do
+    is_expected.to have_many(:subordinates).class_name("Employee").with_foreign_key("manager_id").dependent(:nullify)
+  end
+
+  it "勤怠情報Attendanceを複数もち、" do
+    is_expected.to have_many(:attendances)
+  end
+
+
+
   it 'enumで定義していないroleを持つユーザーを弾くかどうか' do
     expect(user_1).to be_valid
     expect(user_2_manager).to be_valid
     expect(user_3_admin).to be_valid
     expect { user_99_super_super_admin }.to raise_error(ArgumentError, "'99' is not a valid role")
   end
-
-
   it 'has_request_attendancesでrequestを持つattendanceを取得できるか' do
     user_1.attendances.create(worked_on: '2026-05-01')
     user_1.attendances.create(worked_on: '2026-05-02')
