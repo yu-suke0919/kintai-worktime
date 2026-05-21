03e336e6185baeb7852b3ed387bf3d24ce34ed6e
yu-suke0919 <yusukekk919@gmail.com>
2026-05-21 11:39:47 +0900

    不要なアクションを削除

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,23 +1,13 @@
 class EmployeesController < ApplicationController
   before_action :authenticate_employee!
   before_action :set_employee, only: [ :show, :edit, :update, :destroy ]
-  def index
-    @employee = current_employee
-  end
-
   def show
-  end
-
-  def new
   end
 
   def edit
   end
 
   def update
-  end
-
-  def destroy
   end
 
   private
