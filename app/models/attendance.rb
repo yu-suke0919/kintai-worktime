abec12de610a28df7c5992351f4ced6295f1f52e
yu-suke0919 <yusukekk919@gmail.com>
2026-05-26 11:35:05 +0900

    打刻状態を示すenumを追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,15 @@
 class Attendance < ApplicationRecord
+  enum :status, {
+  not_clocked: 0,
+  working: 1,
+  normal: 2,
+  abnormal: 3,
+  absent: 4,
+  paid_leave: 5,
+  holiday: 6,
+  corrected: 20
+}
+
   belongs_to :employee
   has_one :attendance_edit_request
 end
