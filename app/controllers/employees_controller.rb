15328b07215aa97c08d29a371ba9d5c160ed3acc
yu-suke0919 <yusukekk919@gmail.com>
2026-05-26 13:35:40 +0900

    belong_to関連によってfindで取得する必要がなくなったため、manager_name変数を削除

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,6 @@ class EmployeesController < ApplicationController
   before_action :authenticate_employee!
   before_action :set_employee, only: [ :show, :edit, :update ]
   def show
-    @manager_name = @employee.manager_id ? Employee.find(@employee.manager_id).name : "未設定"
   end
 
   def edit
