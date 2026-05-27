20a71a40176daca3968fbde2af529e8e57015815
yu-suke0919 <yusukekk919@gmail.com>
2026-05-27 11:48:14 +0900

    モデルのインスタンスメソッドを追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -11,5 +11,29 @@ class Attendance < ApplicationRecord
 }
 
   belongs_to :employee
-  has_one :attendance_edit_request
+  has_one :attendance_edit_request, dependent: :destroy
+
+  def stamp_start!(time: Time.current)
+    raise ArgumentError, "すでに出勤済みです" if original_started_at.present?
+    raise ArgumentError, "不正な打刻です" if status != "not_clocked"
+    update!(started_at: time, original_started_at: time, status: "working")
+  end
+
+  def stamp_finish!(time: Time.current)
+    raise ArgumentError, "すでに退勤済みです" if original_finished_at.present?
+    raise ArgumentError, "不正な打刻です" if status != "working"
+    update!(finished_at: time, original_finished_at: time, status: "normal")
+  end
+
+  def stamp_break_start!(time: Time.current)
+    raise ArgumentError, "すでに打刻済みです" if original_break_started_at.present?
+    raise ArgumentError, "不正な打刻です" if status != "working"
+    update!(break_started_at: time, original_break_started_at: time)
+  end
+
+  def stamp_break_finish!(time: Time.current)
+    raise ArgumentError, "すでに打刻済みです" if original_break_finished_at.present?
+    raise ArgumentError, "不正な打刻です" if status != "working"
+    update!(break_finished_at: time, original_break_finished_at: time)
+  end
 end
