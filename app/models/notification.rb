f61a8055ec2b526ec2a4da9edda8051600d9ba29
yu-suke0919 <yusukekk919@gmail.com>
2026-05-28 10:43:12 +0900

    通知種別を示すintegerカラムにenumを追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,9 @@
 class Notification < ApplicationRecord
   belongs_to :recipient_employee, table_name: "Employee"
   belongs_to :notifiable, polymorphic: true
+
+  enum :notification_type, {
+    approved: 1,
+    rejected: 2
+  }
 end
