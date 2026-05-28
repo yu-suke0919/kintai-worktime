e1bed912aa46e98178200b94377f9e30f278edab
yu-suke0919 <yusukekk919@gmail.com>
2026-05-28 10:43:33 +0900

    indexとshowアクションに追記

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,9 @@
 class NotificationsController < ApplicationController
   def index
+    @notifications = current_employee.notifications
   end
 
   def show
+    @notification = current_employee.notifications.find(params[:id])
   end
 end
