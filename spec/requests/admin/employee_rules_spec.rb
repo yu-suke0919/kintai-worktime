ed79050498335f4c724136c9f2a94d56dfc5d561
yu-suke0919 <yusukekk919@gmail.com>
2026-06-01 11:39:39 +0900

    自動生成された誤ったテストを削除

--- spec/requests/admin/employee_rules_spec.rb
+++ spec/requests/admin/employee_rules_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Admin::EmployeeRules", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/admin/employee_rules/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET employee/new" do
+  #   it "returns http success" do
+  #     get "/admin/employee_rules/new"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/employee_rules/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  # describe "GET /index" do
+  #   it "returns http success" do
+  #     get "/admin/employee_rules/index"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
