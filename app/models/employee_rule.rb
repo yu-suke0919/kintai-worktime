5c148d62f2c8fb29a1d09c3104ba6bf40152dd56
yu-suke0919 <yusukekk919@gmail.com>
2026-06-02 11:36:22 +0900

    曜日マスクを機能させるメソッドやstaticなマスクを追加

--- app/models/employee_rule.rb
+++ app/models/employee_rule.rb
@@ -1,3 +1,25 @@
 class EmployeeRule < ApplicationRecord
   belongs_to :employee
+  WEEKDAY_MASK ={
+    sunday: 1,
+    monday: 2,
+    tuesday: 4,
+    wednesday: 8,
+    thursday: 16,
+    friday: 32,
+    saturday: 64
+  }.freeze
+  def in_office_days
+    days = []
+    EmployeeRule::WEEKDAY_MASK.each do |day, mask|
+      days.push(day) if self.required_workdays_mask & mask == mask
+    end
+    days
+  end
+
+  def in_office_days=(value)
+    self.required_workdays_mask = value
+      .map { |key|EmployeeRule::WEEKDAY_MASK[key] }
+      .inject(:+)
+  end
 end
