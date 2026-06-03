5bd637c6028a84733ad066581e2ece5ee09cf346
Makakiza <yusukekk919@gmail.com>
2026-06-04 00:37:03 +0900

    newアクションを定義

--- app/controllers/employee_work_date_exception_requests_controller.rb
+++ app/controllers/employee_work_date_exception_requests_controller.rb
@@ -1,5 +1,7 @@
 class EmployeeWorkDateExceptionRequestsController < ApplicationController
   def new
+    @employee = current_employee
+    @exception_request = EmployeeWorkDateExceptionRequest.new()
   end
 
   def edit
