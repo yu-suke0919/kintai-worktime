1e0882a8b2396cf6ff0d2e18a3a925accbfb097f
yu-suke0919 <yusukekk919@gmail.com>
2026-06-03 14:54:21 +0900

    EmployeeWorkDateExceptionRequestsコントローラー生成コマンドによる自動生成

---
+++ spec/requests/employee_work_date_exception_requests_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "EmployeeWorkDateExceptionRequests", type: :request do
+  describe "GET /new" do
+    it "returns http success" do
+      get "/employee_work_date_exception_requests/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/employee_work_date_exception_requests/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
