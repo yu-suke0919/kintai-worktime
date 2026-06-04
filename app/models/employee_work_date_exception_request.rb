954287336a579d9cbef5f7712f3f4da2c41a64bc
yu-suke0919 <yusukekk919@gmail.com>
2026-06-04 12:15:24 +0900

    enumの定義を追加

--- app/models/employee_work_date_exception_request.rb
+++ app/models/employee_work_date_exception_request.rb
@@ -2,4 +2,14 @@ class EmployeeWorkDateExceptionRequest < ApplicationRecord
   belongs_to :employee
 
   has_many :notifications, as: :notifiable, dependent: :nullify
+
+  enum :request_type, {
+    paid_leave: 10,
+    hospitalization: 11,
+    special_leave: 12
+  }
+
+  def self.request_options
+    request_types.map { |k, _| [ I18n.t("enums.employee_work_date_exception_request.request_type.#{k}"), k ] }.to_h
+  end
 end
