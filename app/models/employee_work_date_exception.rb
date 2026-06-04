a17bfe7c6e9d997019f1a056de58cc2a21908283
Makakiza <yusukekk919@gmail.com>
2026-06-05 01:44:52 +0900

    enumを追加

--- app/models/employee_work_date_exception.rb
+++ app/models/employee_work_date_exception.rb
@@ -1,3 +1,9 @@
 class EmployeeWorkDateException < ApplicationRecord
   belongs_to :employee
+
+    enum :exception_type, {
+    paid_leave: 10,
+    hospitalization: 11,
+    special_leave: 12
+  }
 end
