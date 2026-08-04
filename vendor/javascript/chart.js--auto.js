456ecbdeaae1b0f9a51bc20e20c336c1aac59910
yu-suke0919 <yusukekk919@gmail.com>
2026-08-04 11:06:22 +0900

    feat:bin/importmap pin chart.js/autoによるchart.jsの導入

---
+++ vendor/javascript/chart.js--auto.js
@@ -0,0 +1,4 @@
+// chart.js/auto@4.5.1 downloaded from https://ga.jspm.io/npm:chart.js@4.5.1/auto/auto.js
+
+import{Chart as e,registerables as r}from"../dist/chart.js";export{Animation,Animations,ArcElement,BarController,BarElement,BasePlatform,BasicPlatform,BubbleController,CategoryScale,Colors,DatasetController,Decimation,DomPlatform,DoughnutController,Element,Filler,Interaction,Legend,LineController,LineElement,LinearScale,LogarithmicScale,PieController,PointElement,PolarAreaController,RadarController,RadialLinearScale,Scale,ScatterController,SubTitle,TimeScale,TimeSeriesScale,Title,Tooltip,_adapters,_detectPlatform,animator,controllers,elements,layouts,plugins,registry,scales}from"../dist/chart.js";export{T as Ticks,d as defaults}from"../_/MwoWUuIu.js";import"@kurkle/color";e.register(...r);export{e as Chart,e as default,r as registerables};
+
