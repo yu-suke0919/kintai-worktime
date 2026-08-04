fc149c9797b403db72d39244d1141998d12bbf55
yu-suke0919 <yusukekk919@gmail.com>
2026-08-04 13:39:33 +0900

    feat: グラフ表示を追加(リファレンスのダミーグラフです)

---
+++ app/javascript/controllers/attendance_chart_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import Chart from "chart.js/auto"
+
+// Connects to data-controller="attendance-chart"
+export default class extends Controller {
+  connect() {
+    this.chart = new Chart(this.element, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+}
+}
