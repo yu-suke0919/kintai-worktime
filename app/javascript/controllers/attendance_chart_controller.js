e86a7268da7fb6064a5f8aaae9c4dfe311eaf7a5
yu-suke0919 <yusukekk919@gmail.com>
2026-08-05 11:48:41 +0900

    fix:インデントを調整

--- app/javascript/controllers/attendance_chart_controller.js
+++ app/javascript/controllers/attendance_chart_controller.js
@@ -5,22 +5,22 @@ import Chart from "chart.js/auto"
 export default class extends Controller {
   connect() {
     this.chart = new Chart(this.element, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
         }
       }
-    }
-  })
-}
+    })
+  }
 }
