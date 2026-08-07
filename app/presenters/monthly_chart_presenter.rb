912f4fd1d694b7da65045d709ac52d230ad0d4a9
yu-suke0919 <yusukekk919@gmail.com>
2026-08-07 14:46:55 +0900

    fix:日付を1,2,3...管理していたが、月初と翌月初の1日が重複するため、YY-MM-DD形式で管理するように変更

--- app/presenters/monthly_chart_presenter.rb
+++ app/presenters/monthly_chart_presenter.rb
@@ -1,17 +1,16 @@
 class MonthlyChartPresenter
   def create(employee, date)
-    target_dates = 1..date.end_of_month.day
-    attendances_by_date = employee.attendances.where(worked_on: date.beginning_of_month..date.end_of_month).index_by { |attendance|attendance.worked_on.to_date.day }
-    Rails.logger.debug attendances_by_date
-    target_dates.map do |day|
-      attendance = attendances_by_date[day]
+    target_dates = date.beginning_of_month..date.end_of_month
+    attendances = employee.attendances.where(worked_on: date.beginning_of_month..date.end_of_month).index_by { |attendance|attendance.worked_on.to_date }
+    result = target_dates.map do |date|
+      attendance = attendances[date]
 
 
-      if attendance.present?
+      if attendance&.started_at.present?
         workdate_zerohour = attendance.worked_on.in_time_zone.beginning_of_day
         {
           attendance_id: attendance.id,
-          date: day,
+          date: date,
           started_minutes: ((attendance.started_at - workdate_zerohour).to_i)/60,
           finished_minutes: ((attendance.finished_at - workdate_zerohour).to_i)/60,
           break_started_minutes: ((attendance.break_started_at - workdate_zerohour).to_i)/60,
@@ -20,7 +19,7 @@ class MonthlyChartPresenter
       else
         {
           attendance_id: nil,
-          date: day,
+          date: date,
           started_minutes: nil,
           finished_minutes: nil,
           break_started_minutes: nil,
@@ -28,5 +27,19 @@ class MonthlyChartPresenter
         }
       end
     end
+
+    if result.last[:finished_minutes] > 1440
+      result.push(
+        {
+          attendance_id: nil,
+          date: (date.end_of_month + 1.days),
+          started_minutes: nil,
+          finished_minutes: nil,
+          break_started_minutes: nil,
+          break_finished_minutes: nil
+        }
+      )
+    end
+    result
   end
 end
