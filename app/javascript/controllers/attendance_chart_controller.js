319b326e2c5d42e14474fe123b73df6d6da17c2f
yu-suke0919 <yusukekk919@gmail.com>
2026-08-07 14:47:44 +0900

    feat:グラフのツールチップの表示を追加_勤怠・休憩時間の表示

--- app/javascript/controllers/attendance_chart_controller.js
+++ app/javascript/controllers/attendance_chart_controller.js
@@ -9,20 +9,25 @@ export default class extends Controller {
     //times: Array,
   }
   connect() {
-    console.log(this.labelsValue)
-    const labels = this.labelsValue.map(value => value.date)
+    const attendanceByDate = Object.fromEntries(this.labelsValue.map(value => [value.date,value]))
     const data = {
-      labels: labels,
       datasets: [
         {
           label: '勤務時間',
-          data: this.labelsValue.map(value =>{
+          data: this.labelsValue.flatMap(value =>{
             if(value.finished_minutes <= 1440){
-              return ({x:value.date,y:[value.started_minutes,value.finished_minutes]})
+              return ({x:value.date,y:[value.started_minutes,value.finished_minutes],original:attendanceByDate[value.date]})
             }else{
-              return ({x:value.date,y:[value.started_minutes,1440]},
-                      {x:value.date+1,y:[0,value.finished_minutes-1440]}
-              )
+              let nextDate = new Date(value.date + "T00:00:00")
+              nextDate.setDate(nextDate.getDate() + 1)
+              let nextDateString = [
+                nextDate.getFullYear(),
+                String(nextDate.getMonth() + 1).padStart(2, "0"),
+                String(nextDate.getDate()).padStart(2, "0")
+              ].join("-")
+              return [{x: value.date,y:[value.started_minutes,1440],original:attendanceByDate[value.date]},
+                      {x:nextDateString,y:[0,value.finished_minutes-1440],original:attendanceByDate[value.date]}
+            ]
             }
           }
           ),
@@ -32,8 +37,28 @@ export default class extends Controller {
         },
         {
           label: '休憩時間',
-          data: this.labelsValue.map(value => 
-            [value.break_started_minutes,value.break_finished_minutes]
+          data: this.labelsValue.flatMap(value =>{
+            if(value.break_finished_minutes <= 1440){
+              return ({x:value.date,y:[value.break_started_minutes,value.break_finished_minutes],original:attendanceByDate[value.date]})
+            }else{
+              let nextDate = new Date(value.date + "T00:00:00")
+              nextDate.setDate(nextDate.getDate() + 1)
+              let nextDateString = [
+                nextDate.getFullYear(),
+                String(nextDate.getMonth() + 1).padStart(2, "0"),
+                String(nextDate.getDate()).padStart(2, "0")
+              ].join("-")
+              if(value.break_started_minutes > 1440){
+                return {x:nextDateString,y:[value.break_started_minutes-1440,value.break_finished_minutes-1440],original:attendanceByDate[value.date]}
+              }else{
+                return [{x: value.date,y:[value.break_started_minutes,1440],original:attendanceByDate[value.date]},
+                        {x:nextDateString,y:[0,value.break_finished_minutes-1440],original:attendanceByDate[value.date]}
+                ]
+              }
+
+              
+            }
+          }
           ),
           backgroundColor: "#6ffaff",
           grouped: false,
@@ -47,7 +72,8 @@ export default class extends Controller {
       options: {
         responsive: true,
         interaction: {
-            mode: 'index'
+            mode: 'nearest',
+            intersect: true
         },
         plugins: {
           legend: {
@@ -58,7 +84,23 @@ export default class extends Controller {
             text: '今月の勤務実績'
           },
           tooltip:{
+            displayColors: false,
             enabled: true,
+            callbacks: {
+              title: (items) => {
+                return items[0].raw.original.date + "開始の勤怠"
+              },
+              label: (context) =>{
+                const data = context.raw.original
+                const startedTime = this.formatTime(data.started_minutes)
+                const finishedTime = this.formatTime(data.finished_minutes)
+                const breakStartedTime = this.formatTime(data.break_started_minutes)
+                const breakFinishedTime = this.formatTime(data.break_finished_minutes)
+                return [`勤務時間:${startedTime}~${finishedTime}`,
+                        `休憩時間:${breakStartedTime}~${breakFinishedTime}`
+                ]
+              }
+            }
           }
         },
         scales: {
@@ -76,12 +118,26 @@ export default class extends Controller {
           x:{
             ticks:{
               autoSkip: false,
+              callback: function(value,index,ticks){
+                const date = this.getLabelForValue(value).split("-").map((str) => parseInt(str))
+                if (date[2] == 1){
+                  return String(date[1]) + "/" + String(date[2])
+                }
+                else{
+                  return String(date[2])
+                }
+              }
             }
           }
         }
       }
     };
     this.chart = new Chart(this.canvasTarget, config)
+  }
+  formatTime(totalMinutes){
+    const hour = Math.floor(totalMinutes/60)%24
+    const minutes = totalMinutes%60
+    return `${hour}:${String(minutes).padStart(2,'0')}`
   }
   disconnect() {
     this.chart?.destroy()
