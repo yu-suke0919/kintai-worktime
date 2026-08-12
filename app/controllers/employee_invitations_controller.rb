5aa6ea3bad47e51512a3a59007f8bce35131e280
yu-suke0919 <yusukekk919@gmail.com>
2026-08-12 14:53:36 +0900

    feat:招待リンク期限切れ、向こうの場合のエラー遷移を追加

--- app/controllers/employee_invitations_controller.rb
+++ app/controllers/employee_invitations_controller.rb
@@ -6,7 +6,15 @@ class EmployeeInvitationsController < ApplicationController
   def update
   end
 
+  def expired
+  end
+
   def set_employee
-    @employee = Employee.find_by_token_for(params[:employee_id])
+    @employee = Employee.find_by_token_for(:invitational, params[:token])
+    logger.debug { "招待リンク確認結果: employee_id=#{@employee&.id || 'not_found'}" }
+    logger.debug { "#{params[:token]}" }
+    if @employee.nil?
+      render :expired, status: :gone
+    end
   end
 end
