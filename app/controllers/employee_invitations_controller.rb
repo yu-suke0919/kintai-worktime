da97fc7cee408339fa62fbca8a174ad1a85ae265
yu-suke0919 <yusukekk919@gmail.com>
2026-08-12 14:03:57 +0900

    feat:本登録用ページの@employeeを格納するアクションを追加

--- app/controllers/employee_invitations_controller.rb
+++ app/controllers/employee_invitations_controller.rb
@@ -1,7 +1,12 @@
 class EmployeeInvitationsController < ApplicationController
+  before_action :set_employee
   def edit
   end
 
   def update
+  end
+
+  def set_employee
+    @employee = Employee.find_by_token_for(params[:employee_id])
   end
 end
