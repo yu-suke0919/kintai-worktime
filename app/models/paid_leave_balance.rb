64d58b4088f68b47a687ba4ccce8c1bb7a4c8e58
yu-suke0919 <yusukekk919@gmail.com>
2026-09-04 14:31:31 +0900

    feat:有給残高モデルがemployee_ruleを参照する関連を追加

--- app/models/paid_leave_balance.rb
+++ app/models/paid_leave_balance.rb
@@ -1,4 +1,5 @@
 class PaidLeaveBalance < ApplicationRecord
   belongs_to :paid_leave_grant
+  belongs_to :employee_rule
   has_many :transactions, -> { order(effective_on: :asc, id: :asc) }, class_name: "PaidLeaveTransaction"
 end
