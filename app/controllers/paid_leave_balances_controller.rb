f6396422132b08f6f7e95d322f956447316fa3cb
yu-suke0919 <yusukekk919@gmail.com>
2026-09-04 15:00:25 +0900

    fix:eager_loadingを修正

--- app/controllers/paid_leave_balances_controller.rb
+++ app/controllers/paid_leave_balances_controller.rb
@@ -2,7 +2,7 @@ class PaidLeaveBalancesController < ApplicationController
     before_action :authenticate_employee!
     before_action :set_employee
   def index
-    @grant = @employee.paid_leave_grants.includes(balances: :paid_leave_transactions).find(params[:paid_leave_grant_id])
+    @grant = @employee.paid_leave_grants.includes(:balances).find(params[:paid_leave_grant_id])
   end
 
   def set_employee
