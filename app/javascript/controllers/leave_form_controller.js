df18278abe64ea840df065234190dc0032052745
yu-suke0919 <yusukekk919@gmail.com>
2026-09-14 15:40:25 +0900

    feat:休暇申請時に不要なフィールドが表示されていたのを隠す仕様を追加

--- app/javascript/controllers/leave_form_controller.js
+++ app/javascript/controllers/leave_form_controller.js
@@ -1,19 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["requestType","dateField","startsDatetimeField","endsDatetimeField"]
+  static targets = ["requestType","leaveDateField","startsDatetimeField","endsDatetimeField","balanceDisplay","leaveDateFields","leaveHourFields"]
   connect(){
     this.toggleDateAndDateTime()
   }
 
   toggleDateAndDateTime(){
-    const dateFieldEnabled = this.requestTypeTarget.value === "hourly_paid_leave"
-    this.dateFieldTargets.forEach((target) =>{
-      target.disabled = dateFieldEnabled
+    const hourlyPaidLeaveSelected = this.requestTypeTarget.value === "hourly_paid_leave"
+    const fullDayPaidLeaveSelected = this.requestTypeTarget.value === "paid_leave"
+    const paidLeaveSelected = hourlyPaidLeaveSelected || fullDayPaidLeaveSelected
+
+    //時間選択が必要なとき、時間選択欄を有効化する
+    this.leaveDateFieldTargets.forEach((target) =>{
+      target.disabled = hourlyPaidLeaveSelected
     })
+
+    //日付選択が必要なとき、日付選択欄を有効にする
+    this.startsDatetimeFieldTarget.disabled = !hourlyPaidLeaveSelected
+    this.endsDatetimeFieldTarget.disabled = !hourlyPaidLeaveSelected
+
+    //時間有給or1日有給が選択されていたら有給残高を表示
+    this.balanceDisplayTarget.classList.toggle("d-none",!paidLeaveSelected)
+    //日時選択系なら日時選択フォームを表示
+    this.leaveDateFieldsTarget.classList.toggle("d-none",hourlyPaidLeaveSelected)
+    //時間選択系なら日時選択フォームを表示
+    this.leaveHourFieldsTarget.classList.toggle("d-none",!hourlyPaidLeaveSelected)
+
+
     
-    this.startsDatetimeFieldTarget.disabled = !dateFieldEnabled
-    this.endsDatetimeFieldTarget.disabled = !dateFieldEnabled
+
   }
 
   updateEndTime(){
