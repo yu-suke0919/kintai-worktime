7e37c4cd10b4643e3898d85c4e8f211ad4b391ca
yu-suke0919 <yusukekk919@gmail.com>
2026-09-15 13:37:04 +0900

    fix:モデルのインスタンス変数の定義を修正_有給付与日数を変更不能に変更

--- app/models/paid_leave_grant.rb
+++ app/models/paid_leave_grant.rb
@@ -1,12 +1,17 @@
 class PaidLeaveGrant < ApplicationRecord
-  attr_accessor :granted_days
-  attr_accessor :granted_hours
   after_create :synchronize_paid_leave_balances
 
   belongs_to :employee
   belongs_to :granted_by, class_name: "Employee", foreign_key: :granted_by_id
   has_many :balances, -> { order(effective_from: :asc, id: :asc) }, class_name: "PaidLeaveBalance"
 
+  def granted_days
+    granted_minutes / 480
+  end
+
+  def granted_hours
+    granted_minutes / 60
+  end
 
   def synchronize_paid_leave_balances
     PaidLeaveBalances::Synchronizer.for_new_paid_leave_grant!(self)
