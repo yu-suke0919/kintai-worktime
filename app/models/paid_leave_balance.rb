74d7a75b5e2e8a3540d40e5cb7f61b262671a608
yu-suke0919 <yusukekk919@gmail.com>
2026-09-16 13:20:38 +0900

    feat:バリデーションを追加

--- app/models/paid_leave_balance.rb
+++ app/models/paid_leave_balance.rb
@@ -1,5 +1,14 @@
 class PaidLeaveBalance < ApplicationRecord
   belongs_to :paid_leave_grant
   belongs_to :employee_rule
+
+  validate :effective_from_cannot_be_changed, on: :update
   has_many :transactions, -> { order(effective_on: :asc, id: :asc) }, class_name: "PaidLeaveTransaction"
+
+  private
+
+  def effective_from_cannot_be_changed
+    return unless will_save_change_to_attribute?(:effective_from)
+    errors.add(:effective_from, "は変更不可")
+  end
 end
